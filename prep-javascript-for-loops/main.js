fb77e260422bdc4f9fc6ef26ad3fbcc4311a5f60
Ken Kieu <kenkieu93@gmail.com>
2021-08-11 13:56:27 -0700

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,18 @@
+// create your loops here.
+
+console.log('-- Loop 1 --');
+for (let i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+console.log('-- Loop 2 --');
+
+for (let i = 0; i <= 18; i += 2) {
+  console.log(i);
+}
+
+console.log('-- Loop 3 --');
+
+for (let i = 100; i >= 0; i--) {
+  console.log(`Time till explosion ${i}`);
+}
